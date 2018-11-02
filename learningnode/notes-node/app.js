660289cce4e83c1bf62f68511c6d0c503fe15eee
Rajeswari Devi Namana <raji.namana@gmail.com>
2018-11-01 19:11:23 -0500

    used built in fs module

---
+++ learningnode/notes-node/app.js
@@ -0,0 +1,9 @@
+console.log("starting app")
+// storing the module in a variable, so all functionality will be available by using fs
+const fs=require("fs")
+// creates new file if it doesnt exist and appends text to it
+fs.appendFile("greeting.js","welcome",function(err){
+  if(err){
+    console.console.log("error occured");
+  }
+})
